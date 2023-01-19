811c7f7131e1f45a8cd45ecd5ae758c0d84bd361
jra <joao.amado@tecnico.ulisboa.pt>
2023-01-19 15:33:12 +0000

    Read trace csv with iat instead of loc.

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -8,22 +8,28 @@ print("Importing Scapy Library")
 
 
 class FE:
-    def __init__(self, file_path, limit=np.inf, fm_grace=100000, ad_grace=900000, train_skip=False):
+    def __init__(self, file_path, limit=np.inf, max_autoencoder_size=10, fm_grace=100000,
+                 ad_grace=900000, train_stats=None, train_skip=False, attack=''):
+
         self.path = file_path
         self.limit = limit
         self.parse_type = None  # unknown
         self.tsvin = None  # used for parsing TSV file
         self.scapyin = None  # used for parsing pcap with scapy
+        self.fm_grace = fm_grace
+        self.ad_grace = ad_grace
+        self.train_skip = train_skip
 
         if train_skip:
-            self.curPacketIndx = fm_grace + ad_grace
+            self.curPacketIndx = self.fm_grace + self.ad_grace
         else:
             self.curPacketIndx = 0
 
         # Prep Feature extractor (AfterImage) ###
         maxHost = 100000000000
         maxSess = 100000000000
-        self.nstat = nS.netStat(np.nan, maxHost, maxSess)
+        self.nstat = nS.netStat(np.nan, maxHost, maxSess, max_autoencoder_size,
+                                train_stats, train_skip, attack)
 
         self.__check_csv__()
 
@@ -47,40 +53,44 @@ class FE:
             return [0]
 
         # Parse next packet ###
-        row = self.df_csv.iloc[self.curPacketIndx]
         IPtype = np.nan
-        timestamp = row[0]
-        framelen = row[1]
+        timestamp = self.df_csv.iat[self.curPacketIndx, 0]
+        framelen = self.df_csv.iat[self.curPacketIndx, 1]
         srcIP = ''
         dstIP = ''
-        if row[4] != '':  # IPv4
-            srcIP = row[4]
-            dstIP = row[5]
+        srcIP = self.df_csv.iat[self.curPacketIndx, 4]
+        srcIPv6 = self.df_csv.iat[self.curPacketIndx, 19]
+        if srcIP != '':  # IPv4
+            dstIP = self.df_csv.iat[self.curPacketIndx, 5]
             IPtype = 0
-        elif row[17] != '':  # ipv6
-            srcIP = row[17]
-            dstIP = row[18]
+        elif srcIPv6 != '':  # ipv6
+            srcIP = srcIPv6
+            dstIP = self.df_csv.iat[self.curPacketIndx, 20]
             IPtype = 1
         # UDP/TCP port: concat of strings will result in an OR "[tcp|udp]"
-        srcproto = str(row[6]) + str(row[8])
-        dstproto = str(row[7]) + str(row[9])  # UDP or TCP port
-        srcMAC = row[2]
-        dstMAC = row[3]
+        srcproto = str(self.df_csv.iat[self.curPacketIndx, 8]) + \
+            str(self.df_csv.iat[self.curPacketIndx, 10])
+        dstproto = str(self.df_csv.iat[self.curPacketIndx, 9]) + \
+            str(self.df_csv.iat[self.curPacketIndx, 11])  # UDP or TCP port
+        srcMAC = self.df_csv.iat[self.curPacketIndx, 2]
+        dstMAC = self.df_csv.iat[self.curPacketIndx, 3]
         if srcproto == '':  # it's a L2/L1 level protocol
-            if row[12] != '':  # is ARP
+            arp = self.df_csv.iat[self.curPacketIndx, 14]
+            icmp = self.df_csv.iat[self.curPacketIndx, 12]
+            if arp != '':  # is ARP
                 srcproto = 'arp'
                 dstproto = 'arp'
-                srcIP = row[14]  # src IP (ARP)
-                dstIP = row[16]  # dst IP (ARP)
+                srcIP = self.df_csv.iat[self.curPacketIndx, 16]  # src IP (ARP)
+                dstIP = self.df_csv.iat[self.curPacketIndx, 18]  # dst IP (ARP)
                 IPtype = 0
-            elif row[10] != '':  # is ICMP
+            elif icmp != '':  # is ICMP
                 srcproto = 'icmp'
                 dstproto = 'icmp'
                 IPtype = 0
             # some other protocol
             elif srcIP + srcproto + dstIP + dstproto == '':
-                srcIP = row[2]  # src MAC
-                dstIP = row[3]  # dst MAC
+                srcIP = srcMAC  # src MAC
+                dstIP = dstMAC  # dst MAC
 
         self.curPacketIndx = self.curPacketIndx + 1
         srcproto = srcproto.replace('nan', '')[:-2]
@@ -93,8 +103,8 @@ class FE:
             cur_pkt_stats = self.nstat.updateGetStats(str(IPtype), str(srcMAC), str(dstMAC),
                                                       str(srcIP), srcproto, str(dstIP), dstproto,
                                                       int(framelen), float(timestamp))
-            # if self.curPacketIndx == 1000000:
-            #     self.nstat.reset_stats()
+            if not self.train_skip and self.curPacketIndx == self.fm_grace + self.ad_grace:
+                self.nstat.save_stats()
 
             return [cur_pkt, cur_pkt_stats]
         except Exception as e:
@@ -104,10 +114,10 @@ class FE:
     def parse_pcap(self):
         print('Parsing with tshark...')
         fields = "-e frame.time_epoch -e frame.len -e eth.src -e eth.dst \
-                    -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport \
-                    -e udp.srcport -e udp.dstport -e icmp.type -e icmp.code \
-                    -e arp.opcode -e arp.src.hw_mac -e arp.src.proto_ipv4 \
-                    -e arp.dst.hw_mac -e arp.dst.proto_ipv4 \
+                    -e ip.src -e ip.dst -e ip.len -e ip.proto -e tcp.srcport \
+                    -e tcp.dstport -e udp.srcport -e udp.dstport -e icmp.type \
+                    -e icmp.code -e arp.opcode -e arp.src.hw_mac \
+                    -e arp.src.proto_ipv4 -e arp.dst.hw_mac -e arp.dst.proto_ipv4 \
                     -e ipv6.src -e ipv6.dst"
         cmd = 'tshark -r ' + self.path + ' -T fields ' + \
             fields + ' -E separator=\',\' -E header=y -E occurrence=f > ' + self.path.split('.')[0] + ".csv"
